77cedd4dcf9dafe220e1c05d0bda2084960dfc7a
aabed aabed <71728292+hollywo@users.noreply.github.com>
2021-12-15 09:41:23 +0200

    binary tree

---
+++ BTS/class.h
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+	int data;
+	Node* left;
+	Node* right;
+	Node(int data,Node* left,Node*right){
+		Node::data=data;
+		Node::left=left;
+		Node::right=right;
+	}
+};
+class Tree{
+	Node* root;
+	bool AddEL(Node*root,int);
+public:
+	bool AddEL(int);
+	bool DeleteEl(int);
+	int size();
+	int is_empty();
+	Tree();
+	void operator=(Tree);
+};
